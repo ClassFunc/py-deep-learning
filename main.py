e100fc0d82637083b9faefd9e1e986b3d0b32b2d
duongdampa <duong.dam@classfunc.com>
2023-07-24 18:16:06 +0700

    fix port 5000

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 from app import app
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=5000, reload=True)
