6063c5317d8f1df38b0187ddc10318f91b73b6c7
duongdampa <duong.dam@classfunc.com>
2023-07-25 01:16:57 +0700

    p5000

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 from app import app
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=5006, workers=2)
+    uvicorn.run("main:app", host='0.0.0.0', port=5000, workers=2)
     #for production see more https://www.uvicorn.org/deployment/
 
     # for development uvicorn.run("main:app", host='0.0.0.0', port=5000, reload=True)
